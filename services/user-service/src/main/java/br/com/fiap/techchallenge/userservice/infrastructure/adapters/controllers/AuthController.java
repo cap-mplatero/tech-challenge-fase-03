c9d780ee85ff1d1370dd741b4ec62b3a5485c2f0
joaomorito <joaomorito04@gmail.com>
2026-03-16 19:52:37 -0300

    refactor: update AuthController

--- services/user-service/src/main/java/br/com/fiap/techchallenge/userservice/infrastructure/adapters/controllers/AuthController.java
+++ services/user-service/src/main/java/br/com/fiap/techchallenge/userservice/infrastructure/adapters/controllers/AuthController.java
@@ -1,39 +1,32 @@
 package br.com.fiap.techchallenge.userservice.infrastructure.adapters.controllers;
 
-import br.com.fiap.techchallenge.userservice.application.dtos.CreateUserRequest;
+import br.com.fiap.techchallenge.userservice.application.dtos.AuthResponse;
 import br.com.fiap.techchallenge.userservice.application.dtos.LoginRequest;
-import br.com.fiap.techchallenge.userservice.application.ports.input.AuthenticateUserUseCase;
-import br.com.fiap.techchallenge.userservice.application.ports.input.CreateUserUseCase;
-
-import org.springframework.web.bind.annotation.*;
+import br.com.fiap.techchallenge.userservice.application.dtos.RegisterRequest;
+import br.com.fiap.techchallenge.userservice.application.ports.input.AuthUseCase;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    private final CreateUserUseCase createUserUseCase;
-    private final AuthenticateUserUseCase authenticateUserUseCase;
-
-    public AuthController(CreateUserUseCase createUserUseCase,
-                          AuthenticateUserUseCase authenticateUserUseCase){
-
-        this.createUserUseCase=createUserUseCase;
-        this.authenticateUserUseCase=authenticateUserUseCase;
-
-    }
+    private final AuthUseCase authUseCase;
 
     @PostMapping("/register")
-    public void register(@RequestBody CreateUserRequest request){
-
-        createUserUseCase.execute(request);
-
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authUseCase.register(request));
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest request){
-
-        return authenticateUserUseCase.execute(request);
-
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.ok(authUseCase.login(request));
     }
-
 }
